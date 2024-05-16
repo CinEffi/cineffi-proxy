e6b60beccf3eb79ea1998c908d70edff4c3e51b4
김민희 <gimminhui@gimminhuiui-MacBookPro.local>
2024-05-16 23:07:27 +0900

    이메일 전송 로직 추가

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -14,9 +17,52 @@ def handle_post_request():
         'authCode': auth_code,
         'emailAddress': email_address
     }
+
+    send_email(email_address, auth_code)
     
     # JSON 응답 반환
     return jsonify(response_data), 200
 
+def send_email(recipient_email, auth_code):
+    # SMTP 서버 설정
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    sender_email = 'skwd1012@gmail.com' # 보내는 이메일 주소
+    sender_password = 'tboxbeusjvbysajd' # 보내는 이메일의 비밀번호 또는 앱 비밀번호
+
+    # 이메일 메시지 생성
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = '이메일 인증'
+
+    # 이메일 본문 생성
+    body = ""
+    body += "<h3>요청하신 인증 번호입니다.</h3>"
+    body += f"<h1>{auth_code}</h1>"
+    body += "<h3>감사합니다.</h3>"
+
+    # HTML 본문 첨부
+    msg.attach(MIMEText(body, 'html'))
+
+    try:
+        # SMTP 서버에 연결 및 로그인
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(sender_email, sender_password)
+
+        # 이메일 전송
+        server.send_message(msg)
+        print('Email sent successfully')
+
+        # SMTP 서버 연결 종료
+        server.quit()
+
+    except Exception as e:
+        print(f'Failed to send email: {e}')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
+
+
---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+MAIL_SERVER='smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USERNAME = 'skwd1012@gmail.com'
+MAIL_PASSWORD = 'tboxbeusjvbysajd'
+MAIL_USE_TLS = False  
+MAIL_USE_SSL = True
